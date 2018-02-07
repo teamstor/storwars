960014e57cc3c00abfa0ec6e556c1af2ea2bcd1f
Hannes Mann <hannesmann2000@gmail.com>
2018-02-07 12:36:52 +0100

    Add Faction class

---
+++ TBS/Gameplay/Faction.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace TeamStor.TBS.Gameplay
+{
+	/// <summary>
+	/// Player factions.
+	/// </summary>
+	public enum Faction
+	{
+		Soviet,
+		ARLA,
+		Cartel
+	}
+
+	/// <summary>
+	/// Info about each faction.
+	/// </summary>
+	public static class FactionInfo
+	{
+		public static readonly string DescriptionSoviet = "BLABLABLA";
+		public static readonly string DescriptionARLA = "BLABLABLA... They once shot down a helicopter with only rocks, spears and bows.";
+		public static readonly string DescriptionCartel = "BLABLABLA... Kokain.";
+
+		public static string DescriptionFromEnum(Faction faction)
+		{
+			switch(faction)
+			{
+				case Faction.Soviet:
+					return DescriptionSoviet;
+				case Faction.ARLA:
+					return DescriptionARLA;
+				case Faction.Cartel:
+					return DescriptionCartel;
+			}
+
+			return "?";
+		}
+	}
+}
