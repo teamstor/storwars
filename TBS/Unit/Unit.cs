03160b20226e9f7d5aac94c8e412095b5708afae
Hannes Mann <hannesmann2000@gmail.com>
2018-02-08 11:08:30 +0100

    Add units (NOT DONE)

---
+++ TBS/Unit/Unit.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Xna.Framework;
+using TeamStor.Engine.Graphics;
+using TeamStor.TBS.Gameplay.States;
+
+namespace TeamStor.TBS.Unit
+{
+    /// <summary>
+    /// Base unit class.
+    /// </summary>
+    public abstract class Unit
+    {
+        /// <summary>
+        /// Unit data for this unit.
+        /// </summary>
+        public UnitData UnitData;
+        
+        /// <summary>
+        /// The game session this unit is in.
+        /// </summary>
+        public GameSessionState Session;
+        
+        /// <summary>
+        /// The maximum distance this unit can walk in one turn.
+        /// </summary>
+        public virtual int MaxWalkDistance
+        {
+            get { return 1; }
+        }
+
+        /// <summary>
+        /// Called when it's this units turn.
+        /// </summary>
+        public virtual void OnTurn(long turn)
+        {
+            
+        }
+        
+        /// <summary>
+        /// Called when the player tries to move this unit.
+        /// </summary>
+        /// <returns>If the unit was moved.</returns>
+        public virtual bool OnUnitMove(Point newTile)
+        {
+            // TODO
+            return false;
+        }
+
+        /// <summary>
+        /// Draws this unit.
+        /// </summary>
+        /// <param name="batch">The SpriteBatch to use.</param>
+        public virtual void Draw(SpriteBatch batch)
+        {
+            
+        }
+    }
+}
---
+++ TBS/Unit/UnitData.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using TeamStor.TBS.Gameplay;
+
+namespace TeamStor.TBS.Unit
+{
+    /// <summary>
+    /// Static unit data.
+    /// 16x16
+    /// </summary>
+    public class UnitData
+    {
+        public const string UNITS_TEXTURE = "textures/units.png";
+        
+        /// <summary>
+        /// Available units.
+        /// </summary>
+        public static Dictionary<byte, UnitData> Units { get; } = new Dictionary<byte, UnitData>();
+        
+        /// <summary>
+        /// Unit ID.
+        /// </summary>
+        public byte Id;
+        
+        /// <summary>
+        /// Unit name.
+        /// </summary>
+        public virtual string Name { get; protected set;  }
+        
+        /// <summary>
+        /// Faction that this unit belongs to.
+        /// </summary>
+        public virtual Faction Faction { get; protected set; }
+        
+        /// <summary>
+        /// Tier of HQ needed to create this unit.
+        /// </summary>
+        public virtual int Tier { get; protected set; }
+
+        public UnitData(byte id, string name, Faction faction, int tier)
+        {
+            Id = id;
+            Name = name;
+            Faction = faction;
+            Tier = tier;
+            
+            Units.Add(id, this);
+        }
+        
+        public static UnitData FindByName(Faction faction, string name)
+        {
+            foreach(UnitData data in Units.Values)
+            {
+                if(data.Faction == faction && data.Name == name)
+                    return data;
+            }
+
+            return null;
+        }
+    }
+}
